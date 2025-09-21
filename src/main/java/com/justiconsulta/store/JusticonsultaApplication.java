6657c647f91665d1fd729b685d6ae964f637c585
Jesus <jesusdel1611@gmail.com>
2025-09-20 21:52:36 -0500

    Commit inicial

---
+++ src/main/java/com/justiconsulta/store/JusticonsultaApplication.java
@@ -0,0 +1,13 @@
+package com.justiconsulta.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JusticonsultaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JusticonsultaApplication.class, args);
+    }
+
+}
