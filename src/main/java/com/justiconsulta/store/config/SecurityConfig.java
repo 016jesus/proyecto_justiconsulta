02ca8ecf33c786e3988f15ce66d3357c591f281e
Jesus <jesusdel1611@gmail.com>
2025-09-23 09:37:20 -0500

    solucion cors

--- src/main/java/com/justiconsulta/store/config/SecurityConfig.java
+++ src/main/java/com/justiconsulta/store/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.justiconsulta.store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,5 +27,19 @@ public class SecurityConfig {
         // Si tienes filtros custom como ApiKeyAuthFilter/JwtAuthFilter, evita registrarlos aquí.
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*"); // usa addAllowedOriginPattern para comodines
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        config.setAllowCredentials(false); // obligatorio si usas "*"
+        config.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
